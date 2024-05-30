3a678de090f0bf284aa667a273d81d1d32d4c39e
Dănilă Ștefan-Flavian <danilastefan69@yahoo.ro>
2024-05-31 00:48:35 +0300

    PD: Add support for the LCD
    Signed-off-by: Dănilă Ștefan-Flavian <danilastefan69@yahoo.ro>

--- PulsoximetruDigitalPCLP2.ino
+++ PulsoximetruDigitalPCLP2.ino
@@ -1,13 +1,18 @@
 // Pulsoximetru Digital - Proiect PCLP 2
-// Realizat de: Danila Stefan-Flavian
+// Realizat de: Dănilă Ștefan-Flavian
 // Profesor coordonator: Damian Cătalin
 
+#include <Wire.h> // Libraria pentru I2C
+#include <LiquidCrystal_I2C.h> // Libraria pentru display
+
+// Initializam LCD-ul
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); 
+
 void setup() {
-  // put your setup code here, to run once:
-
+  // Setăm numarul de coloane si linii ale LCD-ului
+  lcd.begin(16, 2);
+  lcd.clear();
+  // Afisăm un mesaj pe LCD
+  lcd.print("Pulsoximetru Digital");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+void loop() {}
